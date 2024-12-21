f2a0620e9c43422bc8c689e32563cb5111d3e501
Santiago Villabona <villabonasantiago@gmail.com>
2024-12-20 23:27:48 -0300

    se agregó el componente LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import Home from './Components/Home'
-import Navbar from './Components/Navbar'
-import CardPizza from './Components/CardPizza'
-import Footer from './Components/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import CardPizza from "./Components/CardPizza";
+import Footer from "./Components/Footer";
+import LoginPage from "./Components/LoginPage";
 
 function App() {
-  
   return (
     <>
-      <Navbar/>
-      <Home />
-      <Footer/>
+      <Navbar />
+      {/* <Home /> */}
+      <LoginPage />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
